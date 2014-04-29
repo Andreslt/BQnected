308bd130aca343e005b38c45b9531796b424c809
= <=>
2014-04-29 17:57:12 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BQnected::Application.config.secret_token = '4b3cfab06ef908ab39be3f5049ae2a109b38d7da79912fa2a0cd84c93d44aabfd152ed530156307ebd4966c82be154bb053cd1a854e137100529cd06096cb5ba'
